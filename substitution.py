f580902f63dfd9bc72790334c0e14d179537bc87
JJ Weber <jjweber@gmail.com>
2013-07-28 16:03:17 -0700

    Add a function for making subsitutions

--- substitution.py
+++ substitution.py
@@ -1,13 +1,37 @@
-def find_subs(targets, word_list, n=7):
-    """Using the provided word list, find words n steps from the target words."""
+def find_subs(targets: "set", word_list: "list", n=7):
+    """Using the provided word list and target set, yields a tuple (target_word, target_word + n).
+
+    """
+    from itertools import cycle
+
     subs = {}
 
-    for word, i in zip(word_list, range(len(word_list))):
-        if targets.get(word) is not None:
+    for i, word in enumerate(cycle(word_list)):
+        if word in targets:
             subs[i + n] = word
 
         target = subs.get(i)
         if target is not None:
-            targets[target] = word
+            yield (target, word)
 
-    return targets
+
+def perf_subs(text: "string", subs: "generator"):
+    """Performs substitutions on the text, using the generated (target, replacement) tuples,
+    returns a transformed copy of the text.
+
+    """
+    from string import Template
+    import re
+
+    tmpl_map = {}
+    tmpl_text = text.strip()  # Copy the string
+
+    for target, replacement in subs:
+        r_target = r"\b%s\b" % target
+        tmpl_target = "$" + target
+        tmpl_map[target] = replacement
+        tmpl_text = re.sub(r_target, tmpl_target, tmpl_text)
+
+    tmpl = Template(tmpl_text)
+    transformed_text = tmpl.substitute(tmpl_map)
+    return transformed_text
--- test_substitution.py
+++ test_substitution.py
@@ -1,22 +1,42 @@
 import os
 import unittest
 
-from substitution import find_subs
-
 
 class TestSubsitutionFunctions(unittest.TestCase):
     def setUp(self):
-        self.word_list = open('words').read().splitlines()
-        self.sample_targets = {"sky": "", "Zyzzogeton": ""}
+        words = open('words')
+        self.word_list = words.read().splitlines()
+        words.close()
+        self.sample_targets = set(["sky", "Zyzzogeton",])
 
     def test_find_subs(self):
         """It should return a result map with word -> word + 7, wrapping around the dictionary
         if need be.
-        """
 
-        res = find_subs(self.sample_targets, self.word_list)
-        self.assertEqual(res["sky"], "skylark")
-        self.assertEqual(res["Zyzzogeton"], "Aani")
+        """
+        from substitution import find_subs
+
+        res_gen = find_subs(self.sample_targets, self.word_list)
+
+        res = next(res_gen)
+        self.assertEqual(res[0], "sky")
+        self.assertEqual(res[1], "skylark")
+
+        res = next(res_gen)
+        self.assertEqual(res[0], "Zyzzogeton")
+        self.assertEqual(res[1], "Aani")
+
+
+    def test_perf_subs(self):
+        """It should return a text with the appropriate replacements, and one replacement per word.
+
+        """
+        from substitution import perf_subs
+
+        res_gen = [("sky", "skylark"), ("skylark", "skyphoi")]  # Dynamically equivalent to a generator
+
+        res = perf_subs("The sky is a skylark", res_gen)
+        self.assertEqual(res, "The skylark is a skyphoi")
 
 
 if __name__ == '__main__':
